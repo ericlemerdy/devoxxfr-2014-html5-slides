f9aca586e4364a810d71e081f99e91994b837774
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-04-08 08:45:07 +0200

    Fix git plugin installation

--- jenkins-vagrant/manifests/default.pp
+++ jenkins-vagrant/manifests/default.pp
@@ -14,9 +14,28 @@ file { "/etc/default/jenkins":
   require => Package [ "jenkins" ]
 }
 
-exec { "/usr/bin/wget -O /var/lib/jenkins/plugins/git.jpi http://updates.jenkins-ci.org/latest/git.jpi":
-  creates => "/var/lib/jenkins/plugins/git.jpi",
-  user    => "root",
+exec { "/usr/bin/wget -O /var/lib/jenkins/plugins/git.hpi http://updates.jenkins-ci.org/latest/git.hpi":
+  creates => "/var/lib/jenkins/plugins/git.hpi",
+  user    => "jenkins",
+  notify  => Service [ "jenkins" ],
+  require => Exec [
+    "/usr/bin/wget -O /var/lib/jenkins/plugins/git-client.hpi http://updates.jenkins-ci.org/latest/git-client.hpi",
+    "/usr/bin/wget -O /var/lib/jenkins/plugins/scm-api.hpi http://updates.jenkins-ci.org/latest/scm-api.hpi"
+  ]
+}
+
+exec { "/usr/bin/wget -O /var/lib/jenkins/plugins/git-client.hpi http://updates.jenkins-ci.org/latest/git-client.hpi":
+  creates => "/var/lib/jenkins/plugins/git-client.hpi",
+  user    => "jenkins",
+  require => Package [
+    "jenkins",
+    "git"
+  ]
+}
+
+exec { "/usr/bin/wget -O /var/lib/jenkins/plugins/scm-api.hpi http://updates.jenkins-ci.org/latest/scm-api.hpi":
+  creates => "/var/lib/jenkins/plugins/scm-api.hpi",
+  user    => "jenkins",
   require => Package [
     "jenkins",
     "git"
