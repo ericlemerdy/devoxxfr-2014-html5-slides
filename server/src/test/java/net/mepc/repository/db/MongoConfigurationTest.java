4aaa3fa9ea70f28ac1973dd05ea308bbde799173
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-04-11 11:25:53 +0200

    Simplify test

--- server/src/test/java/net/mepc/repository/db/MongoConfigurationTest.java
+++ server/src/test/java/net/mepc/repository/db/MongoConfigurationTest.java
@@ -5,15 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
-import com.mongodb.MongoClientURI;
-
 public class MongoConfigurationTest {
 	@Test
 	public void with_no_address_and_no_port_should_return_default_uri() {
-		MongoClientURI mongoURI = new MongoClientURI("mongodb://127.0.0.1:27017");
-
 		MongoConfiguration configuration = createConfiguration();
 
-		assertThat(configuration.getUri().getURI()).isEqualTo(mongoURI.getURI());
+		assertThat(configuration.getUri().getURI()).isEqualTo("mongodb://127.0.0.1:27017");
 	}
 }
