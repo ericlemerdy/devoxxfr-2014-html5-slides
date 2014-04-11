a46d39c1fc6337ea3b4b0037b9d56f1629d92a2c
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-04-11 11:44:19 +0200

    Remove unused variable

--- server/src/main/java/net/mepc/resources/AbstractResource.java
+++ server/src/main/java/net/mepc/resources/AbstractResource.java
@@ -2,12 +2,9 @@ package net.mepc.resources;
 
 import org.slf4j.Logger;
 
-import net.mepc.server.ServerConfig;
-
 
 public abstract class AbstractResource {
 
-	protected ServerConfig serverConfig;
 	protected Logger logger;
 
 	public void setLogger(Logger logger) {
